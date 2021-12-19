90cf70eb26ddd88abee9216a4bf74e70d459c22e
aliahmedcs <aliahmedcs1@gmail.com>
2021-12-19 22:49:02 +0200

    first commit to main branch

---
+++ lib/lang/ar_EG.dart
@@ -0,0 +1,11 @@
+const Map<String, String> arEG = {
+  'phone': 'أدخل رقم الهاتف',
+  'login': 'تسجيل الدخول',
+  'platform':'منصة المعلنين',
+  'forgetPassword':'نسيت كلمة المرور',
+  'newAccount':'انشاء حساب جديد',
+  'or':'أو',
+  '':'',
+
+
+};
