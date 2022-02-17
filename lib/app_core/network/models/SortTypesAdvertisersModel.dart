040369f3e5e87f610510a425bcfd614c62d441e7
Zezo12345 <aliahmedcs1@gmail.com>
2022-02-17 20:30:55 +0200

    fix updated account status fixed

---
+++ lib/app_core/network/models/SortTypesAdvertisersModel.dart
@@ -0,0 +1,29 @@
+import 'package:advertisers/app_core/network/models/PeriodModel.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import 'Area.dart';
+part 'SortTypesAdvertisersModel.g.dart';
+
+@JsonSerializable()
+class SortTypesAdvertisersModel {
+  String? reply_speed;
+  String? oldest;
+  String? latest;
+  String? top_rated;
+  String? most_ads;
+  String? most_followers;
+  String? less_folloers;
+  SortTypesAdvertisersModel({
+    this.reply_speed,
+    this.oldest,
+    this.latest,
+    this.top_rated,
+    this.most_ads,
+    this.most_followers,
+    this.less_folloers,
+  });
+
+  factory SortTypesAdvertisersModel.fromJson(Map<String, dynamic> json) => _$SortTypesAdvertisersModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SortTypesAdvertisersModelToJson(this);
+}
